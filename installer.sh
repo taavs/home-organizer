23a4ae81ffc3a2b57b5a300835cf77ce3a658eba
Luis Otavio <luisonasc@gmail.com>
2021-09-09 22:54:08 -0300

    WIP: importing dotfiles

--- installer.sh
+++ installer.sh
@@ -5,14 +5,48 @@ display_help()
     echo "TODO"
 }
 
+import_dotfiles()
+{
+    #Look for files in specific locations
+    #.vimrc 
+    VIMRC=~/.vimrc
+    if [ -f "$VIMRC" ]; then
+		echo "Copying .vimrc"
+		cp ~/.vimrc ./stash/.vimrc
+    else
+		echo "No .vimrc found, skipping"
+    fi
+}
+
+export_dotfiles()
+{
+    echo "TODO"
+}
+
+find_or_create_stash()
+{
+	echo "Welcome back, lets organize your home"
+	
+	STASH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+	STASH="${STASH}/stash"
+	if [ ! -d "$STASH" ]; then
+		echo "Creating stash folder at $STASH"
+		mkdir ./stash
+	else
+		echo "Stash found at $STASH"
+	fi
+}
+
+
+find_or_create_stash
+
 prompt="What would you like me to do?"
 options=("Import" "Export" "Help")
-echo "Welcome back, lets organize your home"
 PS3="$prompt"
 select opt in "${options[@]}" "Quit"; do
     case "$REPLY" in 
-    1) echo "You picked $opt which is option 1";;
-    2) echo "You picked $opt which is option 2";;
+    1) import_dotfiles;;
+    2) export_dotfiles;;
     3) display_help;;
     $((${#options[@]}+1))) echo "Goodbye!"; break;;
     *) echo "Invalid option. Try another one.";continue;;
